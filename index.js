8b00bbac411f6c2574935f9688d92533c0f8d43c
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-04-30 14:55:13 +0300

    Project setup

---
+++ index.js
@@ -0,0 +1,7 @@
+// Canvas ve context tanımlamaları
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// Canvas büyüklük ayarı
+canvas.width = innerWidth;
+canvas.height = innerHeight;
