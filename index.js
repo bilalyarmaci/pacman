778c821fb352f9b562f78a33a04e1c8af0735b6c
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-04-30 19:32:03 +0300

    Add pacman with movement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-// Canvas büyüklük ayarı
+// Canvas büyüklük ayarlaması
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
@@ -11,6 +11,7 @@ class Boundary {
     // Nesne üretmeden ulaşılabilen genişlik ve yükseklik parametreleri
     static width = 40;
     static height = 40;
+    // Canvas'a çizmek için gerekli olacak pozisyon (x, y) ve genişlil-yükseklik parametrelerini ayarlayan yapıcı (constructor)
     constructor({ position }) {
         this.position = position;
         this.width = Boundary.width;
@@ -23,7 +24,35 @@ class Boundary {
     }
 }
 
-// Sınırları temsil eden harita.
+// Pacman sınıfı
+class Pacman {
+    // Boundary sınıfındaki yapıcının benzeri
+    // Daire oluşturulduğundan yarıçap (radius) değeri de gereklidir
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 15;
+    }
+    // Nesneyi ("pacman"i) ekrana çizdiren metot
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = 'yellow';
+        ctx.fill();
+        ctx.closePath();
+    }
+    // Hareket efekti için pozisyon (x ve y koordinatları) güncellemesi yaparak çizdiren metodu çağıran metot
+    move() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
+// Sınırı oluşturan blok parçalarını tutan dizi, yani sınırın ta kendisi
+const boundaries = [];
+
+// Sınırları temsil eden harita
 // '-' sembolü sınırı oluşturan blok parçacığını (kare) temsil ediyor.
 const map = [
     ['-', '-', '-', '-', '-', '-'],
@@ -31,10 +60,7 @@ const map = [
     ['-', ' ', '-', '-', ' ', '-'],
     ['-', ' ', ' ', ' ', ' ', '-'],
     ['-', '-', '-', '-', '-', '-']
-]
-
-// Sınırı oluşturan blok parçalarını tutan dizi, yani sınırın ta kendisi.
-const boundaries = []
+];
 
 // Harita satırlarını tarar
 // row = satır, i = mevcut satır numarası
@@ -51,10 +77,53 @@ map.forEach((row, i) => {
                 }));
                 break;
         }
-    })
-})
+    });
+});
 
-// Diziyi gezinerek her bir elemanın ilgili metodu çağırır
-boundaries.forEach(boundary => {
-    boundary.draw();
-})
+// Pacman nesnesi oluşturuluyor
+const pacman = new Pacman({
+    // Koordinatlar sınırın (duvarın) içinde kalacak şekilde sınır koordinatlarının değerleri üzerinden veriliyor
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity: { // Başlangıç hızı yok - hareket yok
+        x: 0,
+        y: 0
+    }
+});
+
+// Olay dinleyicinin tanımlanması 
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        // Diyagonal bir hareket olmaması için bir eksende sürat varken diğer eksendeki sürat değeri sıfırlanıyor
+        case 'a': 
+            pacman.velocity.x = -2;
+            pacman.velocity.y = 0;
+            break;
+        case 'd':
+            pacman.velocity.x = 2;
+            pacman.velocity.y = 0;
+            break;
+        case 'w':
+            pacman.velocity.y = -2;
+            pacman.velocity.x = 0;
+            break;
+        case 's':
+            pacman.velocity.y = 2;
+            pacman.velocity.x = 0;
+            break;
+    }
+});
+
+
+// Canvas içerisindeki tüm çizimlerin tekrarlı bir biçimde çağırılmasıyla animasyon görünümü oluşturmakta sorumlu metot
+function animate() {
+    requestAnimationFrame(animate); // Animasyon için halihazırda var olan fonksiyon
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Bir önceki çizimlerin silinmesini sağlayan fonksiyon
+    // Diziyi gezinerek her bir elemanın ilgili metodunu çağırır (Duvarları çizdirir)
+    boundaries.forEach(boundary => { boundary.draw(); });
+    pacman.move(); // pacman nesnesinin hareketi için ilgili metot çağırısı
+}
+
+animate();
